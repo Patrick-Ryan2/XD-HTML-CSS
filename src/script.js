a76d3dffe320b08829911735daf5f6afa39febcc
Patrick-Ryan2 <79230859+Patrick-Ryan2@users.noreply.github.com>
2021-11-19 03:05:05 +1300

    Add html and css for Web A3

--- src/script.js
+++
@@ -1,6 +0,0 @@
-var btn = document.querySelector('#nav-btn');
-var nav = document.querySelector('.nav');
-var listArea = document.querySelector('.list');
-var menuItems = document.querySelector('.left-col').innerHTML;
-listArea.innerHTML = menuItems;
-
