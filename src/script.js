5ea75af14d3fbd369cb46616c62fa5e25285157f
Patrick-Ryan2 <79230859+Patrick-Ryan2@users.noreply.github.com>
2021-11-18 22:34:09 +1300

    removed dist moved code files into src

---
+++ src/script.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('#nav-btn');
+var nav = document.querySelector('.nav');
+var listArea = document.querySelector('.list');
+var menuItems = document.querySelector('.left-col').innerHTML;
+listArea.innerHTML = menuItems;
+
